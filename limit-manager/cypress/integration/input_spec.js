55490bfdbb782eb1eec5fb30e584712469d99e25
Felipe Orlando <fobsouza@gmail.com>
2018-11-09 20:22:41 -0200

    Add integration test for input

---
+++ limit-manager/cypress/integration/input_spec.js
@@ -0,0 +1,64 @@
+describe('Input Test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/')
+  })
+
+  context('when change the value', () => {
+    beforeEach(() => {
+      cy.get('.input__field')
+        .clear()
+        .type(4000)
+        .blur()
+    })
+
+    it('should change the label with the total subtracted text', () => {
+      cy.get('.label strong').should('contain', '1000')
+    })
+
+    it('should change the range with the value', () => {
+      cy.get('.range__slider').should('have.value', '4000')
+    })
+  })
+
+  context('when change the value to above to the max number', () => {
+    beforeEach(() => {
+      cy.get('.input__field')
+        .clear()
+        .type(6000)
+        .blur()
+    })
+
+    it('should change the input value to the max number', () => {
+      cy.get('.input__field').should('have.value', '5000')
+    })
+
+    it('should change the label with the total subtracted text', () => {
+      cy.get('.label strong').should('contain', '0')
+    })
+
+    it('should change the range with the value', () => {
+      cy.get('.range__slider').should('have.value', '5000')
+    })
+  })
+
+  context('when change the value to lower than 0', () => {
+    beforeEach(() => {
+      cy.get('.input__field')
+        .clear()
+        .type(-1)
+        .blur()
+    })
+
+    it('should change the input value to 0', () => {
+      cy.get('.input__field').should('have.value', '0')
+    })
+
+    it('should change the label with the total subtracted text', () => {
+      cy.get('.label strong').should('contain', '5000')
+    })
+
+    it('should change the range with the value', () => {
+      cy.get('.range__slider').should('have.value', '0')
+    })
+  })
+})
