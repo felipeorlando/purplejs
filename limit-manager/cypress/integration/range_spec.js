5e46290e010f8e80998060ece6e2ef2de3ebbf83
Felipe Orlando <fobsouza@gmail.com>
2018-11-09 20:31:31 -0200

    Add test for input range

---
+++ limit-manager/cypress/integration/range_spec.js
@@ -0,0 +1,22 @@
+describe('Range Test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/')
+  })
+
+  context('when change the value', () => {
+    beforeEach(() => {
+      cy.get('.range__slider')
+        .as('range')
+        .invoke('val', 4000)
+        .trigger('change')
+    })
+
+    it('should change the input with the value', () => {
+      cy.get('.input__field').should('have.value', '4000')
+    })
+
+    it('should change the label with the total subtracted text', () => {
+      cy.get('.label strong').should('contain', '1000')
+    })
+  })
+})
